8400f18b24cfc03075e3618765f31fdbfb6274ba
Deedo67 <70287389+Deedo67@users.noreply.github.com>
2021-06-06 23:36:37 -0400

    Initial commit

---
+++ src/main/java/MPE15/example/ex15.java
@@ -0,0 +1,16 @@
+package MPE15.example;
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Derek Gilberti
+ */
+
+//class begin
+public class ex15 {
+    //main method begin
+    public static void main(String[] args) {
+
+    }
+}
