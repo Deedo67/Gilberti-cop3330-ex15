e05b48e2040b38e987f7efbaab5b4a4a6589088b
Deedo67 <70287389+Deedo67@users.noreply.github.com>
2021-06-06 23:44:15 -0400

    Commit Second

--- src/main/java/MPE15/example/ex15.java
+++ src/main/java/MPE15/example/ex15.java
@@ -11,6 +11,26 @@ import java.util.Scanner;
 public class ex15 {
     //main method begin
     public static void main(String[] args) {
+        //scanner
+        Scanner in = new Scanner(System.in);
 
+        //input
+        System.out.print("What is the username? ");
+        String username = in.nextLine();
+        System.out.print("What is the password? ");
+        String password = in.nextLine();
+
+        //known variable
+        String pass = "abc$123";
+
+        //Output
+        if (password.equals(pass))
+        {
+            System.out.println("Welcome!");
+        }
+        else
+        {
+            System.out.println("I don't know you.");
+        }
     }
 }
